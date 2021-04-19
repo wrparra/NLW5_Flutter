262d15d1679b526b792d3ed2c069815d957fbcbd
Wellington Rocha Parra <wrparra@wrsoft.com.br>
2021-04-19 15:00:21 -0300

    [UPDATE] Aula 01

---
+++ DevQuiz/lib/home/widgets/app_bar/app_bar_widget.dart
@@ -0,0 +1,48 @@
+import 'package:DevQuiz/core/app_gradients.dart';
+import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/home/widgets/score_card/score_card_widget.dart';
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget()
+      : super(
+            preferredSize: Size.fromHeight(250),
+            child: Container(
+              height: 250,
+              child: Stack(
+                children: [
+                  Container(
+                    height: 161,
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    width: double.maxFinite,
+                    decoration: BoxDecoration(gradient: AppGradients.linear),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text.rich(
+                          TextSpan(
+                              text: "Olá, ",
+                              style: AppTextStyles.title,
+                              children: [
+                                TextSpan(
+                                    text: "Wellington",
+                                    style: AppTextStyles.titleBold)
+                              ]),
+                        ),
+                        Container(
+                          width: 48,
+                          height: 48,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "https://avatars.githubusercontent.com/u/687490?s=60&v=4"))),
+                        )
+                      ],
+                    ),
+                  ),
+                  Align(alignment: Alignment(0, 1), child: ScoreCardWidget()),
+                ],
+              ),
+            ));
+}
